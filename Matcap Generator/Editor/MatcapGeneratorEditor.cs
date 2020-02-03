fd4c02353a21fad422032e7b9d4af79f9af53252
bomul0327 <bomul0327@naver.com>
2020-02-04 00:15:42 +0900

    Init

---
+++ Matcap Generator/Editor/MatcapGeneratorEditor.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace UnrealChan
+{
+    [CustomEditor(typeof(MatcapGenerator))]
+    public class MatcapGeneratorEditor : Editor
+    {
+        SerializedProperty texturePropertiesIdxProp;
+
+        Texture2D texture;
+
+        void OnEnable()
+        {
+            texturePropertiesIdxProp = serializedObject.FindProperty("CurrentTexturePropertiesIdx");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            serializedObject.Update();
+
+            MatcapGenerator script = target as MatcapGenerator;
+
+            texture = script.GeneratedTexture;
+
+            if (script.TargetMaterial != null)
+            {
+                var properties = script.TargetMaterial.GetTexturePropertyNames();
+                texturePropertiesIdxProp.intValue = EditorGUILayout.Popup("Target Texture Property", texturePropertiesIdxProp.intValue, properties);
+                script.TextureProperty = properties[texturePropertiesIdxProp.intValue];
+            }
+
+            EditorGUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Save Matcap Texture"))
+            {
+                if (script.TextureProperty != string.Empty)
+                {
+                    var fullPath = script.SaveTexture(texture);
+                    AssetDatabase.Refresh();
+
+                    Texture2D saved = (Texture2D)AssetDatabase.LoadAssetAtPath(fullPath, typeof(Texture2D));
+
+                    script.ApplyToMaterial(saved);
+                    script.TextureProperty = string.Empty;
+                }
+            }
+
+            EditorGUILayout.EndHorizontal();
+
+            GUILayout.Label("Generated Matcap Preview");
+            if (texture != null)
+            {
+                GUILayout.Label(texture);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Matcap Generator/MatcapGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[ExecuteInEditMode]
+public class MatcapGenerator : MonoBehaviour
+{
+    public int Size = 512;
+
+    public string folderPath = "Assets/MatcapTextures/";
+
+    public string FileName = "Default";
+
+    public Material TargetMaterial;
+
+    [HideInInspector]
+    public string TextureProperty;
+
+    [HideInInspector]
+    public int CurrentTexturePropertiesIdx;
+
+    [HideInInspector]
+    public Texture2D GeneratedTexture;
+
+    private int texturePropertyID;
+
+    private Camera cam;
+
+    void OnEnable()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        if (TextureProperty != string.Empty)
+        {
+            GeneratedTexture = Capture();
+            ApplyToMaterial(GeneratedTexture);
+        }
+    }
+
+    public Texture2D Capture()
+    {
+        RenderTexture rt = new RenderTexture(Size, Size, 24);
+        cam.targetTexture = rt;
+
+        cam.Render();
+        RenderTexture.active = rt;
+
+        Texture2D texture = new Texture2D(Size, Size, TextureFormat.RGB24, false);
+        Rect rect = new Rect(0, 0, Size, Size);
+        texture.ReadPixels(rect, 0, 0);
+        texture.Apply();
+
+        RenderTexture.active = null;
+
+        cam.targetTexture = null;
+        rt.Release();
+        rt = null;
+
+        return texture;
+    }
+
+    public void ApplyToMaterial(Texture2D texture)
+    {
+        if (TargetMaterial == null) return;
+        texturePropertyID = Shader.PropertyToID(TextureProperty);
+        TargetMaterial.SetTexture(texturePropertyID, texture);
+    }
+
+    public string SaveTexture(Texture2D texture)
+    {
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+
+        byte[] bytes = texture.EncodeToPNG();
+
+        var filePath = folderPath + FileName + TextureProperty + "_" + System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+
+        File.WriteAllBytes(filePath + ".png", bytes);
+
+        return filePath + ".png";
+    }
+}
